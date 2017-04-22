91ff9f3e3fcbabd44d12c9aaa525d73381328fb0
Mario Faúndez <mariofaundez@hotmail.com>
2017-04-22 02:31:14 -0300

    configuration drone.io with chef docker (for testing purpose)

--- cookbooks/docker_drone/recipes/default.rb
+++ cookbooks/docker_drone/recipes/default.rb
@@ -23,3 +23,32 @@ docker_service_manager_systemd 'default' do
   host 'unix:///var/run/docker.sock'
   action :start
 end
+
+docker_image 'drone/drone' do
+  action :pull
+end
+
+directory "/drone" do
+  mode '0755'
+  owner 'root'
+  group 'root'
+  action :create
+  recursive true
+end 
+
+# Run container exposing ports
+docker_container 'drone-server' do
+  repo 'drone/drone'
+  port '80:8000'
+  volumes [ '/drone:/var/lib/drone/' ]
+  restart_policy 'always'
+  env ['DRONE_OPEN=true', 'DRONE_GITHUB=true', 'DRONE_GITHUB_CLIENT=1a', 'DRONE_GITHUB_SECRET=1a2b3c', 'DRONE_SECRET=123']
+end
+
+docker_container 'drone-agent' do
+  repo 'drone/drone'
+  volumes [ '/var/run/docker.sock:/var/run/docker.sock' ]
+  command 'agent'
+  restart_policy 'always'
+  env ['DRONE_SERVER=ws://drone-server:8000/ws/broker', 'DRONE_SECRET=123']
+end
